9b026a3af36b5160647dbcdd3e31acd90fb7196e
Carl Pearson <pearson@illinois.edu>
2018-03-21 15:02:36 -0500

    First pass unblocked transfer times

--- src/cmd/analyze.rs
+++ src/cmd/analyze.rs
@@ -5,14 +5,32 @@ use std::io::BufReader;
 use std::time::SystemTime;
 use std::cmp::Ordering;
 
-// use self::heteroprof::{decode_document, Document};
+use interval::Interval;
+
+impl Interval for heteroprof::Compute {
+    fn lb(&self) -> f64 {
+        return self.start;
+    }
+    fn ub(&self) -> f64 {
+        return self.lb() + self.dur;
+    }
+}
+
+impl Interval for heteroprof::Transfer {
+    fn lb(&self) -> f64 {
+        return self.start;
+    }
+    fn ub(&self) -> f64 {
+        return self.lb() + self.dur;
+    }
+}
 
 pub fn run(path: &str) {
     let start = SystemTime::now();
     let metadata = fs::metadata(path);
     let file = fs::File::open(path).unwrap();
     let mut reader = BufReader::new(file);
-    let doc = heteroprof::decode_document(&mut reader).unwrap();
+    let mut doc = heteroprof::decode_document(&mut reader).unwrap();
     eprintln!("{} computes", doc.computes().len());
     eprintln!("{} transfers", doc.transfers().len());
 
@@ -24,7 +42,45 @@ pub fn run(path: &str) {
     eprintln!("{}s elapsed", secs);
     eprintln!("{}MB/s", sz as f64 / secs / 1024 as f64 / 1024 as f64);
 
-    doc.computes().sort_by(|a, b| -> Ordering {
+    // sort computes
+    doc.computes_mut().sort_by(|a, b| -> Ordering {
         return a.cmp_start(b);
     });
+
+    // sort transfers
+    doc.transfers_mut().sort_by(|a, b| -> Ordering {
+        return a.cmp_start(b);
+    });
+
+    let mut ci = 0 as usize;
+
+    let mut freet = 0 as f64;
+
+    // find all transfers not covered by a c
+    for t in doc.transfers() {
+        // advance c until we're covered or it's past us
+        let mut c = &doc.computes()[ci];
+
+        while ci < doc.computes().len() {
+            c = &doc.computes()[ci];
+            eprintln!("c is to {}-{}", c.lb(), c.ub());
+            if c.overlaps(t) {
+                eprintln!(
+                    "t={}-{} overlapped by c={}-{}",
+                    t.lb(),
+                    t.ub(),
+                    c.lb(),
+                    c.ub()
+                );
+                break;
+            }
+            if c.lb() >= t.ub() {
+                eprintln!("t={}-{} unblocked", t.lb(), t.ub());
+                freet += t.ub() - t.lb();
+                break;
+            }
+            ci += 1;
+        }
+    }
+    eprintln!("{}", freet / 1e9);
 }
