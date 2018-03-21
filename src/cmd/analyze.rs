ab311f8db1feb9437bf05ca3602610743c066e09
Carl Pearson <pearson@illinois.edu>
2018-03-21 13:57:33 -0500

    print computes and transfers

--- src/cmd/analyze.rs
+++ src/cmd/analyze.rs
@@ -1,15 +1,25 @@
 extern crate heteroprof;
 
-use std::fs::File;
+use std::fs;
 use std::io::BufReader;
+use std::time::SystemTime;
 
-use self::heteroprof::{decode_document, Document};
+// use self::heteroprof::{decode_document, Document};
 
 pub fn run(path: &str) {
-    println!("analyzing!!!");
-
-    let file = File::open(path).unwrap();
+    let start = SystemTime::now();
+    let metadata = fs::metadata(path);
+    let file = fs::File::open(path).unwrap();
     let mut reader = BufReader::new(file);
     let doc = heteroprof::decode_document(&mut reader).unwrap();
-    println!("{}", doc.computes().len());
+    eprintln!("{} computes", doc.computes().len());
+    eprintln!("{} transfers", doc.transfers().len());
+
+    // print some info about execution
+    let sz = metadata.unwrap().len();
+    let dur = start.elapsed().unwrap();
+    let secs = (dur.as_secs() as f64) + (dur.subsec_nanos() as f64) / 1e9;
+    eprintln!("{} MB", sz / 1024 / 1024);
+    eprintln!("{}s elapsed", secs);
+    eprintln!("{}MB/s", sz as f64 / secs / 1024 as f64 / 1024 as f64);
 }
